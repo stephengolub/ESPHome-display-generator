78782cb8d456b5e634694cbeb8bd5513b538815b
Stephen Golub <nickburns2006@gmail.com>
2021-06-01 14:41:01 -0500

    feat(tools): adding in base for tool usage.
    Currently only supports line, rectangle, and individual toggling of
    pixels. The output is still raw on/off of the pixel, not the actual line
    drawing. That will come soon.

--- README.md
+++ README.md
@@ -19,12 +19,23 @@ Click a cell to highlight it. Once your picture is set, you can click the genera
 
 ## Limitations
 
-Currently, it is a point-by-point drawing. Ultimately the desire is to calculate or track lines and shapes.
+### Tools
+
+There are only the three tools implemented:
+* Pixel Toggle (pencil)
+* Line drawing
+* Rectangle
+
+_As of now, none of these will create the proper code, they still do the "turn pixel on" code._
+
+### Known Bugs
+
+There's an issue that I know is a decimal point or some mundane details that is causing the lines when the slope is negative to be offset weirdly. So if anyone has any ideas, PRs are welcome. I'll be tracking this as issue #1.
 
 ## TODO
 
+* Fix line offset weirdness with negative slopes.
 * Drawing tools:
-    * Line
     * Circle/Ellipse
-    * Rectangle/Square
+* Track lines drawn and actually generate the `it.line` or `it.rectangle` code
 * Colors
--- js/code.js
+++ js/code.js
@@ -3,15 +3,13 @@ const minWidth = 8;
 const maxHeight = 128;
 const maxWidth = 128;
 
-const fillSelect = (id, min, max) => {
-  const sel = document.getElementById(id);
-  for (let i = min; i <= max; i++) {
-    const opt = document.createElement('option');
-    opt.value = i;
-    opt.innerHTML = i;
-    sel.appendChild(opt);
-  }
-};
+const STATE_ON = "on";
+const STATE_OFF = "off";
+
+const TOOL_PENCIL = "pencil";
+const TOOL_LINE = "line";
+const TOOL_ELLIPSE = "ellipse";
+const TOOL_RECTANGLE = "rectangle";
 
 let grid = [];
 
@@ -24,12 +22,34 @@ const copyRow = (destRow, srcRow) => {
 
 let mouseDown = false;
 let destination = undefined;
+let currentTool = TOOL_PENCIL;
+let point1 = undefined;
+let point2 = undefined;
+let drawPoints = [];
+
+const resetToolPoints = () => {
+  point1 = undefined;
+  point2 = undefined;
+  drawPoints = [];
+};
+
+const chooseTool = (toolName) => {
+  if ([TOOL_PENCIL, TOOL_LINE, TOOL_ELLIPSE, TOOL_RECTANGLE].indexOf(toolName) >= 0) {
+    document.getElementById(`tool-${currentTool}`).classList.remove("active");
+    document.getElementById(`tool-${toolName}`).classList.add("active");
+    currentTool = toolName;
+    resetToolPoints();
+  }
+};
 
 const targetIsCell = (e) => e.target.classList.contains("cell");
 const onGridClick = (e) => {
   if (targetIsCell(e)) {
-    e.target.classList.toggle("on");
-    e.target.classList.toggle("off");
+    const cell = e.target;
+    if (currentTool == TOOL_PENCIL) {
+      cell.classList.toggle(STATE_ON);
+      cell.classList.toggle(STATE_OFF);
+    }
   }
 };
 
@@ -37,51 +57,189 @@ const setInfoBox = (text) => {
   document.getElementById("info").innerText = text;
 };
 
+const validPoint = (coords) => {
+  return (coords[0] >= 0 && coords[0] <= maxWidth) && (coords[1] >= 0 && coords[1] <= maxHeight);
+}
+
+const lineBetweenPoints = (pointA, pointB) => {
+  x1 = pointA[0];
+  y1 = pointA[1];
+  x2 = pointB[0];
+  y2 = pointB[1];
+
+  points = [];
+  if (x2 == x1) { // Vertical Line
+    for (let y = y1; y <= y2 && validPoint([x1, y]); y++) {
+      points.push([x1, y]);
+    }
+  } else if (y2 == y1) { // Horizontal Line
+    for (let x = x1; x <= x2 && validPoint([y1, x]); x++) {
+      points.push([x, y1]);
+    }
+  } else {
+    let m = (y2 - y1) / (x2 - x1);
+    console.log("Slope:", m);
+    let xMin = x1;
+    let xMax = x2;
+    let b = y1;
+    let y = y2;
+    if (x1 > x2) {
+      xMax = x1;
+      xMin = x2;
+    }
+    if (y1 > y2) {
+      y = y1;
+      b = y2;
+    }
+    for (let x = xMin; x <= xMax && validPoint([x, y]); x++) {
+      y = m*(x - xMin) + b;
+      points.push([Math.abs(Math.round(x)), Math.abs(Math.round(y))]);
+    }
+  }
+  return points;
+};
+
+const rectAroundPoints = (pointA, pointB) => {
+  x1 = pointA[0];
+  y1 = pointA[1];
+  x2 = pointB[0];
+  y2 = pointB[1];
+
+  points = [];
+  if (x2 == x1 || y2 == y1) { // Vertical or Horizontal Line
+    return lineBetweenPoints(pointA, pointB);
+  } else {
+    let xMin = x1;
+    let xMax = x2;
+    let yMin = y1;
+    let yMax = y2;
+    if (x1 > x2) {
+      xMax = x1;
+      xMin = x2;
+    }
+    if (y1 > y2) {
+      yMax = y1;
+      yMin = y2;
+    }
+    for (x = xMin; x <= xMax; x++) {
+      if (x == xMin || x == xMax) {
+        for (y = yMin; y <= yMax; y++) {
+          points.push([x,y]);
+        }
+      }
+      points.push([x,yMin]);
+      points.push([x,yMax]);
+    }
+  }
+  return points;
+}
+
 const onGridMouseDown = (e) => {
   if (targetIsCell(e)) {
-    destination = e.target.classList.contains("off");
-    setInfoBox(destination.toString());
+    const cell = e.target;
+    destination = cell.classList.contains(STATE_OFF);
     mouseDown = true;
-    setCellValue(e.target, destination);
+    switch (currentTool) {
+      case TOOL_PENCIL:
+        setCellValue(cell, destination);
+        break;
+      case TOOL_LINE:
+        point1 = coordsFromCell(cell);
+        break;
+      case TOOL_ELLIPSE:
+        point1 = coordsFromCell(cell);
+        break;
+      case TOOL_RECTANGLE:
+        point1 = coordsFromCell(cell);
+        break;
+    }
   }
 };
 
-const onGridMouseUp = (e) => {
+const onGridMouseUp = () => {
   mouseDown = false;
   destination = undefined;
-  setInfoBox("");
+  resetToolPoints();
+  switch (currentTool) {
+    case TOOL_PENCIL:
+      break;
+    case TOOL_LINE:
+      break;
+    case TOOL_ELLIPSE:
+      break;
+    case TOOL_RECTANGLE:
+      break;
+  }
 };
 
 const coordsFromCell = cell => {
   return cell.id.split("_").map(x => parseInt(x, 10));
 };
 
-const setCellValue = (cell, destination) => {
-  coords = coordsFromCell(cell);
-  setInfoBox(`${coords}: ${destination}`);
-  grid[coords[1]][coords[0]] = destination;
+const setCellValueByCoords = (coords, state, cell) => {
+  const x = coords[0];
+  const y = coords[1];
+  grid[y][x] = state;
+  if (cell == undefined) {
+    cell = document.getElementById(`${x}_${y}`);
+  }
+  if (state) {
+    cell.classList.add(STATE_ON)
+    cell.classList.remove(STATE_OFF)
+  } else {
+    cell.classList.add(STATE_OFF)
+    cell.classList.remove(STATE_ON)
+  }
+};
+
+const setCellValue = (cell, state) => {
+  setCellValueByCoords(coordsFromCell(cell), state, cell);
+};
+
+const setBulkCellValueByCoords = (points, state) => {
+  points.map(c => setCellValueByCoords(c, state));
+};
+
+const getCellValue = (coords) => {
+  console.log("Cell:", coords)
+  return grid[coords[1]][coords[0]];
 };
 
 const onGridMouseOver = (e) => {
   if (mouseDown && targetIsCell(e)) {
     const cell = e.target;
-    if (destination) {
-      cell.classList.add("on")
-      cell.classList.remove("off")
-    } else {
-      cell.classList.add("off")
-      cell.classList.remove("on")
+    switch (currentTool) {
+      case TOOL_PENCIL:
+        setCellValue(cell, destination);
+        break;
+      case TOOL_LINE:
+        point2 = coordsFromCell(cell);
+        setBulkCellValueByCoords(drawPoints, false);
+        drawPoints = lineBetweenPoints(point1, point2).filter(c => !getCellValue(c));
+        for (let i in points) {
+          setCellValueByCoords(points[i], true);
+        }
+        break;
+      case TOOL_ELLIPSE:
+        break;
+      case TOOL_RECTANGLE:
+        point2 = coordsFromCell(cell);
+        setBulkCellValueByCoords(drawPoints, false);
+        drawPoints = rectAroundPoints(point1, point2).filter(c => !getCellValue(c));
+        for (let i in points) {
+          setCellValueByCoords(points[i], true);
+        }
+        break;
     }
-    setCellValue(cell, destination);
   }
 }
 
 const getCell = (state, x, y) => {
-  const cls = state ? "on" : "off";
+  const cls = state ? STATE_ON : STATE_OFF;
   const cell = document.createElement('div');
   cell.id = `${x}_${y}`;
   cell.classList.add("cell");
-  cell.classList.add(state ? "on" : "off");
+  cell.classList.add(state ? STATE_ON : STATE_OFF);
   return cell;
 };
 
@@ -122,9 +280,6 @@ const onFieldChange = () => {
 };
 
 const onReady = () => {
-  fillSelect("width", minWidth, maxWidth);
-  fillSelect("height", minHeight, maxHeight);
-
   const params = new URLSearchParams(window.location.search);
 
   const w = params.get("w");
@@ -168,3 +323,4 @@ const generate = () => {
 const copyToClipboard = () => {
   navigator.clipboard.writeText(document.getElementById("output").innerText);
 };
+
