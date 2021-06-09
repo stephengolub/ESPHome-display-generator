2e077ecc4d67b28319ab8f0d3badbaf77bf850c7
Stephen Golub <nickburns2006@gmail.com>
2021-06-09 10:05:19 -0500

    fix(copy): attempt at fixing copy to clipboard

--- js/code.js
+++ js/code.js
@@ -407,7 +407,24 @@ const generate = () => {
 };
 
 
-const copyToClipboard = () => {
-  navigator.clipboard.writeText(document.getElementById("output").innerText);
-};
+function copyToClipboard(el) {
+  var el = document.getElementById("output"),
+    oldContentEditable = el.contentEditable,
+    oldReadOnly = el.readOnly,
+    range = document.createRange();
 
+    el.contentEditable = true;
+    el.readOnly = false;
+    range.selectNodeContents(el);
+
+    var s = window.getSelection();
+    s.removeAllRanges();
+    s.addRange(range);
+
+    el.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+
+    el.contentEditable = oldContentEditable;
+    el.readOnly = oldReadOnly;
+
+    document.execCommand('copy');
+}
